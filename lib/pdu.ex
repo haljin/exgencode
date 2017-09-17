3724cc708098da4dba281f97bd5b9c9300f2dc6a
haljin <paul.shinobi@gmail.com>
2017-09-17 21:38:33 +0200

    Fix for docs

--- lib/pdu.ex
+++ lib/pdu.ex
@@ -13,26 +13,26 @@ defmodule Exgencode.Pdu do
   Encode the Elixir structure into a binary give the protocol version.
   
   ### Examples:
-    iex> Exgencode.Pdu.encode(%TestPdu.PzTestMsg{otherTestField: 100})
-    << 1 :: size(12), 100 :: size(24), 15 :: size(8), 10 :: size(24) >>
+      iex> Exgencode.Pdu.encode(%TestPdu.PzTestMsg{otherTestField: 100})
+      << 1 :: size(12), 100 :: size(24), 15 :: size(8), 10 :: size(24) >>
 
-    iex> Exgencode.Pdu.encode(%TestPdu.PzTestMsg{testField: 99, otherTestField: 100})
-    << 99 :: size(12), 100 :: size(24), 15 :: size(8), 10 :: size(24) >>
+      iex> Exgencode.Pdu.encode(%TestPdu.PzTestMsg{testField: 99, otherTestField: 100})
+      << 99 :: size(12), 100 :: size(24), 15 :: size(8), 10 :: size(24) >>
 
   Version number can be optionally added to control the encoding of the PDU and exclude certain fields if the version number is lower that specified.
   
-    pdu = %TestPdu.VersionedMsg{newerField: 111, evenNewerField: 7}
-    assert << 10 :: size(16), 111 :: size(8), 14 :: size(8) >> == Exgencode.Pdu.encode(pdu)
-    assert << 10 :: size(16) >> == Exgencode.Pdu.encode(pdu, "1.0.0")
-    assert << 10 :: size(16), 111 :: size(8) >> == Exgencode.Pdu.encode(pdu, "2.0.0")
+      pdu = %TestPdu.VersionedMsg{newerField: 111, evenNewerField: 7}
+      assert << 10 :: size(16), 111 :: size(8), 14 :: size(8) >> == Exgencode.Pdu.encode(pdu)
+      assert << 10 :: size(16) >> == Exgencode.Pdu.encode(pdu, "1.0.0")
+      assert << 10 :: size(16), 111 :: size(8) >> == Exgencode.Pdu.encode(pdu, "2.0.0")
 
   ### Examples:
 
-    iex> Exgencode.Pdu.encode(%TestPdu.VersionedMsg{newerField: 111, evenNewerField: 7}, "1.0.0")
-    << 10 :: size(16) >>
+      iex> Exgencode.Pdu.encode(%TestPdu.VersionedMsg{newerField: 111, evenNewerField: 7}, "1.0.0")
+      << 10 :: size(16) >>
 
-    iex> Exgencode.Pdu.encode(%TestPdu.VersionedMsg{newerField: 111, evenNewerField: 7}, "2.0.0")
-    << 10 :: size(16), 111 :: size(8) >>
+      iex> Exgencode.Pdu.encode(%TestPdu.VersionedMsg{newerField: 111, evenNewerField: 7}, "2.0.0")
+      << 10 :: size(16), 111 :: size(8) >>
   """
   @spec encode(Exgencode.pdu, Version.version) :: binary
   def encode(pdu, version \\ nil), do: Exgencode.Pdu.Protocol.encode(pdu, version)
@@ -44,18 +44,18 @@ defmodule Exgencode.Pdu do
   a mangled binary.
   
   ### Examples:
-    iex> Exgencode.Pdu.decode(%TestPdu.PzTestMsg{}, << 1 :: size(12), 100 :: size(24), 15 :: size(8), 10 :: size(24)>>)
-    {%TestPdu.PzTestMsg{otherTestField: 100}, <<>>}
+      iex> Exgencode.Pdu.decode(%TestPdu.PzTestMsg{}, << 1 :: size(12), 100 :: size(24), 15 :: size(8), 10 :: size(24)>>)
+      {%TestPdu.PzTestMsg{otherTestField: 100}, <<>>}
 
   Version number can be optionally added to control how the decoding function reads the given binary. If the provided version does not match the requirement
   specified in the field definition the given field will be ignored.
   
   ### Examples:
-    iex> Exgencode.Pdu.decode(%TestPdu.VersionedMsg{}, << 10 :: size(16) >>, "1.0.0")
-    {%TestPdu.VersionedMsg{oldField: 10}, <<>>}
+      iex> Exgencode.Pdu.decode(%TestPdu.VersionedMsg{}, << 10 :: size(16) >>, "1.0.0")
+      {%TestPdu.VersionedMsg{oldField: 10}, <<>>}
 
-    iex> Exgencode.Pdu.decode(%TestPdu.VersionedMsg{}, << 10 :: size(16), 111 :: size(8) >>, "2.0.0")
-    {%TestPdu.VersionedMsg{oldField: 10, newerField: 111}, <<>>}
+      iex> Exgencode.Pdu.decode(%TestPdu.VersionedMsg{}, << 10 :: size(16), 111 :: size(8) >>, "2.0.0")
+      {%TestPdu.VersionedMsg{oldField: 10, newerField: 111}, <<>>}
 
   """
   @spec decode(Exgencode.pdu, binary, Version.version) :: {Exgencode.pdu, binary}
