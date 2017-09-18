90b0afa7dd18a775da037069d0891e70eda5bc8e
haljin <paul.shinobi@gmail.com>
2017-09-18 15:27:47 +0200

    Version update

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exgencode.Mixfile do
   def project do
     [
       app: :exgencode,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.5-rc",
       start_permanent: Mix.env == :prod,
       deps: deps(),
