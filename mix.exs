37fbbbf88cc9735bc3d4a8e050bebb45d0377351
Pawel Antemijczuk <pawel.antemijczuk1@motorolasolutions.com>
2018-12-20 10:53:47 +0100

    Bump major version because of Elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exgencode.Mixfile do
   def project do
     [
       app: :exgencode,
-      version: "1.3.0",
+      version: "2.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
