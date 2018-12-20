3fea86e65535cacaf2bbe3ef13ecd418dd45cd03
Pawel Antemijczuk <pawel.antemijczuk1@motorolasolutions.com>
2018-12-20 10:53:28 +0100

    Drop Elixir 1.6 too because I can't be bothered

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exgencode.Mixfile do
     [
       app: :exgencode,
       version: "1.3.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
