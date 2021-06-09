720c0dd993745755a4442e7da7632f2356a269e5
haljin <paul.shinobi@gmail.com>
2021-06-09 11:41:33 +0200

    Dep update

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Exgencode.Mixfile do
       {:ex_doc, "~> 0.20", only: :dev, runtime: false},
       {:credo, "~> 1.0", only: [:dev, :test]},
       {:inch_ex, "~> 2.0", only: :docs},
-      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.1.0", only: [:dev], runtime: false}
     ]
   end
 
